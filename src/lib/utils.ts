8dd924788797761e1b99d20b249d02dc0e3d8a5d
sushil-03 <sushilrawat1720@gmail.com>
2026-06-16 13:46:56 +0530

    chore:fix capitalizeWords

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -38,7 +38,7 @@ export const getReadableDate = (date?: string | null) => {
 export const capitalizeWords = (text: string): string => {
   return text
     .toLowerCase()
-    .split(" ")
+    .split(/[ _-]/)
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 };
